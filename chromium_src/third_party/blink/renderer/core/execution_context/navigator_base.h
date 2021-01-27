johnkennyuk/brave-core

Navigator::userAgennt() moved.
Chromium change:

https://source.chromium.org/chromium/chromium/src/+/8d5f6a7690de8163f6405690d31ea7f18444d691

commit 8d5f6a7690de8163f6405690d31ea7f18444d691
Author: Nate Chapin <japhet@chromium.org>
Date:   Mon Nov 23 18:39:56 2020 +0000

    Move GetUserAgentMetadata() and userAgent() to NavigatorBase

    For userAgent(), all the necessary state can be ready off of
    the ExecutionContext already.

    For GetUserAgentMetadata(), it's already available on
    WorkerGlobalScope and can be easily reached from LocalDOMWindow,
    so add a GetUserAgentMetadata() to ExecutionContext.

chromium_src/third_party/blink/renderer/core/execution_context/navigator_base.h
@@ -1,0 +1,33 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_CORE_EXECUTION_CONTEXT_NAVIGATOR_BASE_H_
+#define BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_CORE_EXECUTION_CONTEXT_NAVIGATOR_BASE_H_
+
+#include "third_party/blink/public/platform/web_content_settings_client.h"
+#include "third_party/blink/renderer/core/execution_context/execution_context.h"
+#include "third_party/blink/renderer/core/frame/local_frame.h"
+#include "third_party/blink/renderer/core/frame/navigator_ua.h"
+#include "third_party/blink/renderer/core/loader/frame_loader.h"
+
+#define userAgent                                                             \
+  userAgent() const override {                                                \
+    if (blink::WebContentSettingsClient* settings =                           \
+            brave::GetContentSettingsClientFor(GetExecutionContext())) {      \
+      if (!settings->AllowFingerprinting(true)) {                             \
+        return brave::BraveSessionCache::From(*(GetExecutionContext()))       \
+            .FarbledUserAgent(DomWindow()->GetFrame()->Loader().UserAgent()); \
+      }                                                                       \
+    }                                                                         \
+    return userAgent_ChromiumImpl();                                          \
+  }                                                                           \
+  String userAgent_ChromiumImpl
+
+#include "../../../../../../../third_party/blink/renderer/core/execution_context/navigator_base.h"
+#undef userAgent
+
+
+
+#endif  // BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_CORE_EXECUTION_CONTEXT_NAVIGATOR_BASE_H_

ally-browser/brave-core-fork

chromium_src/third_party/blink/renderer/core/execution_context/navigator_base.h
@@ -3,21 +3,27 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include <random>
+#ifndef BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_CORE_EXECUTION_CONTEXT_NAVIGATOR_BASE_H_
+#define BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_CORE_EXECUTION_CONTEXT_NAVIGATOR_BASE_H_
 
 #include "third_party/blink/public/platform/web_content_settings_client.h"
+#include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
+#include "third_party/blink/renderer/core/frame/navigator_id.h"
 #include "third_party/blink/renderer/core/loader/frame_loader.h"
 
-#define BRAVE_NAVIGATOR_USERAGENT                                           \
+#define BRAVE_NAVIGATOR_BASE_USER_AGENT                                     \
   if (blink::WebContentSettingsClient* settings =                           \
           brave::GetContentSettingsClientFor(GetExecutionContext())) {      \
     if (!settings->AllowFingerprinting(true)) {                             \
       return brave::BraveSessionCache::From(*(GetExecutionContext()))       \
           .FarbledUserAgent(DomWindow()->GetFrame()->Loader().UserAgent()); \
     }                                                                       \
-  }
+  }                                                                         \
 
-#include "../../../../../../../third_party/blink/renderer/core/frame/navigator.cc"
+#include "../../../../../../../third_party/blink/renderer/core/execution_context/navigator_base.h"
+#undef BRAVE_NAVIGATOR_BASE_USER_AGENT
 
-#undef BRAVE_NAVIGATOR_USERAGENT
+
+
+#endif  // BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_CORE_EXECUTION_CONTEXT_NAVIGATOR_BASE_H_